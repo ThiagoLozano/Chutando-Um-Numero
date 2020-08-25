bcc47d52d8c01746fc2249f953bf07be9147959e
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-08-24 22:51:33 -0300

    Programa Comentado

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ from random import randint
 
 class ChuteUmNumero:
     def __init__(self):
+        # Escolhe um número para o computador.
         self.pc = randint(0, 10)
         self.contador = 0
 
-    def chutar(self):
+    def Chutar(self):
         print("Estou pensando em um número entre 0 e 10!")
+        # Pergunta para o usuário.
         jg = float(input("\nQual número você acha que é: "))
         self.contador += 1
+        # Cria um laço de repetição até o jogador acertar.
         while self.pc != jg:
             print("Errou!")
             if self.pc > jg:
+                # Caso o número digitado seja menor.
                 print("Dica: Pensei em um número maior")
                 jg = float(input("\nQual número você acha que é: "))
                 self.contador += 1
             elif self.pc < jg:
+                # Caso o número digitado seja maior.
                 print("Dica: Pensei em um número menor")
                 jg = float(input("\nQual número você acha que é: "))
                 self.contador += 1
@@ -24,8 +29,9 @@ class ChuteUmNumero:
             if self.contador == 1:
                 print("Uau, acertou de primeira!")
             print("\nAcertou!")
+            # Retorna a quantidade de tentativas.
             print("Número total de Tentativas: {}".format(self.contador))
 
 
 j = ChuteUmNumero()
-j.chutar()
+j.Chutar()
